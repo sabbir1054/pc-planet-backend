400c14d6c4649b70968c16a7935e18495aada752
sabbir1054 <mdsabbir1054@gmail.com>
2023-08-12 18:49:56 +0600

    add pc save and updated backend

--- index.js
+++ index.js
@@ -18,11 +18,23 @@ const run = async () => {
   try {
     const db = client.db("pc-planet");
     const productCollection = db.collection("products");
+    const savedListCollection = db.collection("savePcList");
 
+    app.post("/saveList", async (req, res) => {
+      const data = req.body;
+      const result = savedListCollection.insertOne(data);
+      res.send({ status: true, data: result });
+    });
+    app.get("/product/:id", async (req, res) => {
+      const cursor = productCollection.find({
+        _id: new ObjectId(req.params.id),
+      });
+      const product = await cursor.toArray();
+      res.send({ status: true, data: product });
+    });
     app.get("/products", async (req, res) => {
       const cursor = productCollection.find({});
       const product = await cursor.toArray();
-
       res.send({ status: true, data: product });
     });
 
